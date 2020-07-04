326ae5ebeb59bcfc83d7984f46cb0212aa77b432
trancosowiliam <wileleh@gmail.com>
2020-07-04 16:21:38 -0300

    Added description with test(include icon)

--- app/src/main/java/com/example/eldritchcreator/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/eldritchcreator/presentation/home/HomeFragment.kt
@@ -6,10 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.eldritchcreator.R
 import com.example.eldritchcreator.presentation.BaseFragment
+import com.example.eldritchcreator.util.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        hello.transformationMethod = null
+        hello.text = spannable(
+            normal("Você viu alguma coisa("),
+            image(hello, R.drawable.stats_observation),
+            normal("-1) em caso de sucesso, tente o convencer("),
+            image(hello, R.drawable.stats_influence),
+            normal(") de alguma coisa, caso contrario ("),
+            image(hello, R.drawable.stats_will),
+            normal("+1) para não perder "),
+            image(hello, R.drawable.stats_strength),
+            normal(" ou "),
+            image(hello, R.drawable.stats_lore)
+        )
+    }
 }
---
+++ app/src/main/java/com/example/eldritchcreator/util/spannable_ext.kt
@@ -0,0 +1,80 @@
+package com.example.eldritchcreator.util
+
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.TextUtils
+import android.text.style.*
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import java.lang.IllegalStateException
+
+fun spannable(vararg te: SpannableString) : SpannableString {
+    var spannable = SpannableString("")
+
+    te.forEach {
+        spannable += it
+    }
+
+    return spannable
+}
+
+private fun span(s: CharSequence, vararg spans: Any): SpannableString {
+    val spannable =
+        if (s is String) SpannableString(s)
+        else (s as? SpannableString) ?: SpannableString("")
+
+    spans.forEach {
+        spannable.setSpan(it, 0, spannable.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+
+    return spannable
+}
+
+operator fun SpannableString.plus(s: SpannableString) = SpannableString(TextUtils.concat(this, s))
+operator fun SpannableString.plus(s: String) = SpannableString(TextUtils.concat(this, s))
+
+fun normal(s: CharSequence) = span(s, SpannableString(s))
+fun bold(s: CharSequence) = span(s, StyleSpan(android.graphics.Typeface.BOLD))
+fun italic(s: CharSequence) = span(s, StyleSpan(android.graphics.Typeface.ITALIC))
+fun underline(s: CharSequence) = span(s, UnderlineSpan())
+fun strike(s: CharSequence) = span(s, StrikethroughSpan())
+fun sup(s: CharSequence) = span(s, SuperscriptSpan())
+fun sub(s: CharSequence) = span(s, SubscriptSpan())
+fun size(size: Float, s: CharSequence) = span(s, RelativeSizeSpan(size))
+fun color(color: Int, s: CharSequence) = span(s, ForegroundColorSpan(color))
+fun background(color: Int, s: CharSequence) = span(s, BackgroundColorSpan(color))
+fun url(url: String, s: CharSequence) = span(s, URLSpan(url))
+fun colorBold(s: CharSequence, color: Int) = span(
+    s,
+    ForegroundColorSpan(color),
+    StyleSpan(android.graphics.Typeface.BOLD)
+)
+
+fun image(textView: TextView, resourceId: Int): SpannableString {
+    val drawable =
+        ContextCompat.getDrawable(textView.context, resourceId) ?: throw IllegalStateException()
+    val ratio = drawable.intrinsicHeight.toDouble() / textView.lineHeight.toDouble()
+    val width = drawable.intrinsicWidth.toDouble() / ratio
+
+    drawable.setBounds(0, 0, width.toInt(), textView.lineHeight)
+
+    return span(
+        " ",
+        ImageSpan(drawable, ImageSpan.ALIGN_BOTTOM)
+    )
+}
+
+fun clickable(context: Context, s: CharSequence, color: Int, id: Int = 0, func: (Int) -> Unit) =
+    span(s,
+        object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                func(id)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                ds.color = ContextCompat.getColor(context, color)
+            }
+        })
